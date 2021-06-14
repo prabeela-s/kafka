f5a1981454c2f020c3ab516eaf497947231c4bf0
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2021-06-14 17:09:50 +0530

    Create Settings.java

---
+++ src/main/java/kafka/workshop/Settings.java
@@ -0,0 +1,6 @@
+package kafka.workshop;
+
+public class Settings {
+    public static String BOOTSTRAP_SERVERS = "localhost:9092";
+    public static String SCHEMA_REGISTRY = "http://localhost:8081";
+}
