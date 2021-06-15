e5ad2672d7bad6bdb6a6fef938dabdfe23c0d6ef
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2021-06-15 16:54:38 +0530

    Update Settings.java

--- src/main/java/kafka/workshop/Settings.java
+++ src/main/java/kafka/workshop/Settings.java
@@ -3,4 +3,6 @@ package kafka.workshop;
 public class Settings {
     public static String BOOTSTRAP_SERVERS = "localhost:9092";
     public static String SCHEMA_REGISTRY = "http://localhost:8081";
+    public static String BOOTSTRAP_SERVERS_LIST = "localhost:9092,localhost:9093,localhost:9094,localhost:9095";
+
 }
