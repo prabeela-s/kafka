2101558f2ed17bcfb67fd013f250f88a51ea371f
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2021-06-15 17:49:24 +0530

    Update SimpleConsumer.java

--- src/main/java/kafka/workshop/SimpleConsumer.java
+++ src/main/java/kafka/workshop/SimpleConsumer.java
@@ -17,7 +17,7 @@ import java.util.UUID;
 import static org.apache.kafka.clients.consumer.ConsumerConfig.*;
 
 public class SimpleConsumer {
-    public static String TOPIC = "greetings";
+    public static String TOPIC = "greetings2";
 
     public static void main(String[] args) throws Exception {
         Properties props = new Properties();
@@ -27,10 +27,14 @@ public class SimpleConsumer {
         // Consumer group id, should be unique, partition allocated to consumers inside teh group
         props.put(GROUP_ID_CONFIG, "greetings-consumer-group"); // offset, etc, TODO
 
-        // -- true, automatically commit the offset, automatically
+        // __consumer_offset is topic with 50 partitions used to track offset processed by kafka consumers
+        // once read the message from offset, consumer should commit the offset back to broker on topic __consumer_offset
+
+        // -- true, automatically commit the offset back to broker as soon message is received
+            // RISK: consumer read the message, may be not processed, but still chances that consumer commit to broker stating message processed
         // -- false, developers manually commit the offset
         props.put(ENABLE_AUTO_COMMIT_CONFIG, "false");
-        props.put(AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        props.put(AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000"); // commit every 1 second
 
         props.put(SESSION_TIMEOUT_MS_CONFIG, "30000");
 
@@ -38,13 +42,13 @@ public class SimpleConsumer {
         props.put(KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
 
         // value deserialize the bytes data into String format, JSON,AVRO formats
-
         props.put(VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
 
-        // discussed later
-        //  props.put(ConsumerConfig.GROUP_ID_CONFIG, UUID.randomUUID().toString());
+        // to read all data always
+        //props.put(ConsumerConfig.GROUP_ID_CONFIG, UUID.randomUUID().toString());
         // props.put(ConsumerConfig.CLIENT_ID_CONFIG, "your_client_id");
-        // props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        // From beginning
+        //props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
         // <Key as string, Value as string>
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
@@ -57,9 +61,12 @@ public class SimpleConsumer {
 
         List<PartitionInfo> partitions = consumer.partitionsFor(TOPIC);
         for (PartitionInfo partitionInfo: partitions) {
-            // partitionInfo.
+
             System.out.println("Partition " + partitionInfo);
+            System.out.println("Leader " + partitionInfo.leader());
         }
+
+        System.out.println("---------------------------");
 
         while (true) {
             // Consumer poll for the data with wait time
@@ -80,9 +87,12 @@ public class SimpleConsumer {
 
 
                 // manual commit if ENABLE_AUTO_COMMIT_CONFIG is "false"
-                // technically consumer send a message to broker about commited offset against consumer group
+                // technically consumer send a message to broker about commited offset against consumer group __consumer_groups
+                // when we restart the consumer, it can read from where it left
+
                 consumer.commitSync();
-                Thread.sleep(3000);
+
+                Thread.sleep(1000);
             }
 
             // Thread.sleep(2000);
