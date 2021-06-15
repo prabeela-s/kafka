673b0bff07cf40d8d12766235ac59577cea8ed88
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2021-06-15 17:49:56 +0530

    Update SimpleProducer.java

--- src/main/java/kafka/workshop/SimpleProducer.java
+++ src/main/java/kafka/workshop/SimpleProducer.java
@@ -4,19 +4,19 @@ package kafka.workshop;
 // kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 3 --topic greetings
 // kafka-console-consumer --bootstrap-server localhost:9092 --topic greetings  --from-beginning --property print.key=true --property print.timestamp=true
 
+// kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 3 --topic greetings2
+
+
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
-
 import java.util.Properties;
-
 import static org.apache.kafka.clients.producer.ProducerConfig.*;
- 
 
 public class SimpleProducer {
 
-    public static String TOPIC = "greetings";
+    public static String TOPIC = "greetings2";
 
 
     public static String[] greetingMessages = new String[] {
@@ -155,14 +155,30 @@ public class SimpleProducer {
 
         props.put(BOOTSTRAP_SERVERS_CONFIG, Settings.BOOTSTRAP_SERVERS); // broker address
 
+        // acks = 0 - broker receive the message, message still in memory, not persisted, ack back to producer
+            // FAST
+            // Risk: if the broker fails writing to disk, message shall be lost
+        // acks = 1 - broker receive the message, written to disk [persisted], ack back to producer
+            // Medium, since Disk IO involved
+            // Good: Message is received ,stored in HDD, even if the broker crash, still have message in HDD
+            // Risk: If the system/hdd itself fails, no network, we will lose messages
+        // acks = all - broker receive the message, write to disk [persisted], update replicas [persisted], ack back to producer
+            // SLOW, since Disk IO on all replicas and network traffic between lead broker and replicas
+            // Good: even if one system fails, other system has data, no data loss
         props.put(ACKS_CONFIG, "all"); // acknowledge level "0", "1", "all"
 
+        // when producer send message to broker, if any failures, whether producer should retry or not
         props.put(RETRIES_CONFIG, 2); // how many retry when msg failed to send
 
+        // producer.send(message);  // main thread, internally kafka sdk at producer collect the messages, won't send to broker immediate
+        // Main Thread
+        // Worker threads, is used to send message to broker, they collect the messages based of size/time
+
         // whatever first condition reached,
-        // group messages by max byte size, 16 KB, dispatch when it reaches 16 KB
+        // collect messages by max byte size, 16 KB, dispatch when it reaches 16 KB
         props.put(BATCH_SIZE_CONFIG, 16000); // bytes
-        // group the messages by max wait time, when 100 ms reached, dispatch the message
+
+        // collect the messages by max wait time, when 100 ms reached, dispatch the message
         props.put(LINGER_MS_CONFIG, 100); // milli second
 
         // Reserved memory, pre-alloted in bytes
@@ -183,20 +199,23 @@ public class SimpleProducer {
 
 
         int counter = 0;
-        for (int i = 0 ; i < 1000; i++) {
+        for (int i = 0 ; i < 5000; i++) {
             for (String message:greetingMessages) {
                 // producer record, topic, key (null), value (message)
                 // send message, not waiting for ack
                 String key = "Message" + counter ;
                 String value = counter + " " + message;
+
                 ProducerRecord record = new ProducerRecord<>(TOPIC, key, value);
-                // producer.send(record); // async, non-blocking
+
+                // producer.send(record); // async, non-blocking, it won't wait for broker ack
+
                 RecordMetadata metadata = (RecordMetadata) producer.send(record).get(); // sync, blocking
 
                 System.out.printf("Greeting %d - %s sent\n", counter, message);
                 System.out.println("Ack offset " + metadata.offset() + " partition " + metadata.partition());
 
-                Thread.sleep(100); // Demo only,
+                Thread.sleep(5000); // Demo only,
                 counter++;
             }
         }
