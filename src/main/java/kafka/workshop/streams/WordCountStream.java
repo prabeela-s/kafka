a2c6f89873b78ff7a554bc84d8acf51e75c5906a
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2021-09-07 10:03:28 +0530

    Update WordCountStream.java

--- src/main/java/kafka/workshop/streams/WordCountStream.java
+++ src/main/java/kafka/workshop/streams/WordCountStream.java
@@ -1,76 +1,105 @@
-package kafka.workshop.streams;
+package kafka.workshop;
 
-import kafka.workshop.Settings;
+import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
+import kafka.workshop.models.Invoice;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
-
-import java.util.Arrays;
-import java.util.Properties;
+import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.kstream.*;
 import org.apache.kafka.streams.state.KeyValueBytesStoreSupplier;
 import org.apache.kafka.streams.state.Stores;
 
-// kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 3 --topic texts
-// kafka-topics --create --zookeeper localhost:2181 --replication-factor 1 --partitions 3 --topic word-count
+import java.util.Arrays;
+import java.util.Properties;
 
-// kafka-console-producer --broker-list localhost:9092 --topic texts
+//kafka-topics --zookeeper localhost:2181 --create --topic words --replication-factor 1 --partitions 3
+//kafka-console-producer --broker-list localhost:9092 --topic words
 
-// kafka-console-consumer --bootstrap-server localhost:9092 --topic word-count --from-beginning --property print.key=true --property print.value=true --formatter kafka.tools.DefaultMessageFormatter --property value.deserializer=org.apache.kafka.common.serialization.LongDeserializer
 
 
 public class WordCountStream {
+    static  String bootstrapServers = "localhost:9092";
+    //FIXME: chance schema url
+    static String schemaUrl = "http://localhost:8081";
 
-    public static void main(final String[] args) throws Exception {
-        Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "wordcount-application");
-        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, Settings.BOOTSTRAP_SERVERS);
-        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+    public static <KTable> void main(String[] args) throws  Exception {
 
-        KeyValueBytesStoreSupplier storeSupplier = Stores.inMemoryKeyValueStore("in-mem");
+
+        final Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "word-count-stream");
+        props.put(StreamsConfig.CLIENT_ID_CONFIG, "word-count-stream-client");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+
+
+        props.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 1 * 1000);
+        props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
+
 
         final StreamsBuilder builder = new StreamsBuilder();
+        final KStream<String, String> wordStream = builder.stream("words");
 
-        // kafka stream subscribe from a topic called texts
-        // get lines entered by user/you from kafka-console-producer
-        KStream<String, String> textLines = builder.stream("texts");
-        // flatMapValues shall convert array of elements into elements
-        // for example, ["how", "are", "you"] convert into "how", "are", "you"
-        //KTable, groupBy, count aggregation
-        KTable<String, Long> wordCounts = textLines
-                .flatMapValues(textLine -> Arrays.asList(textLine.toLowerCase().split("\\W+")))
-                .groupBy((key, word) -> word) // here key is null, group data by word like how, are , you, apple, orange
-                .count(Materialized.<String, Long>as(storeSupplier) // does the count of words
-                        .withKeySerde(Serdes.String())
-                        .withValueSerde(Serdes.Long())) ;
+        // apply transformation, topology, processor
+        // transformation: remove white space , upper to lower case
+        final KStream<String, String> filteredStream =  wordStream.map ( (key, value) -> new KeyValue<>(key, value.trim().toLowerCase()))
+                        .filter( (key, value) -> !value.isEmpty());
 
-        // converting ktable stream changes into stream
-        // whenever new word added/updated in ktable, that information is streamed to a topi called "word-count"
-        // values to word-count is producer with key and value where key is a word like apple, orange as String Type
-        // value is count of words which is Long Type
-        wordCounts.toStream().to("word-count", Produced.with(Serdes.String(), Serdes.Long()));
+        // split line into word array
+        final KStream<Object, String[]> splitWordStream = filteredStream.map ( (key, value) -> new KeyValue<>(null, value.split("\\W+")));
 
-        //textLines, wordCounts is known as topology/stream processors
-        
-        // collection of streams put together
+        // now we have word array, we need convert and flatten them into word, not word array
+        // FlatMapValues
+        final KStream<Object, String> indWordStream = splitWordStream.flatMapValues((values) -> Arrays.asList(values));
+
+        KGroupedStream<String, String> groupedStream = indWordStream.groupBy( (key, value) -> value); // group by word, value is java, jvm
+
+        KStream<String, Long> wordCountStream  = groupedStream.count(Materialized.as("wordCount"))
+                        .toStream();
+
+
+
+        splitWordStream.foreach(new ForeachAction<Object, String[]>() {
+            @Override
+            public void apply(Object key, String[] words) {
+                System.out.println("  Key " + key + "  value    " + words.length );
+            }
+        });
+
+
+        filteredStream.foreach(new ForeachAction<String, String>() {
+            @Override
+            public void apply(String key, String line) {
+                System.out.println("  Key " + key + "  value    " + line );
+            }
+        });
+
+        wordCountStream.foreach(new ForeachAction<String, Long>() {
+            @Override
+            public void apply(String key, Long count) {
+                System.out.println("  Key " + key + "  value    " + count );
+            }
+        });
+
+
+
+        indWordStream.foreach(new ForeachAction<Object, String>() {
+            @Override
+            public void apply(Object key, String line) {
+                System.out.println("indWordStream  Key " + key + "  value    " + line );
+            }
+        });
+
+
         final KafkaStreams streams = new KafkaStreams(builder.build(), props);
 
-        try {
-            // clean up existing states if any
-            streams.cleanUp();
-        }catch(Exception e) {
-            System.out.println("error while cleanup states");
-        }
 
-        // start the stream processing, this creates threads, tasks, and run the stream..
         streams.start();
 
-        // graceful shutdown, Ctrl C or Service stop commnd SIGTERM to stohe application gracefully
-        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
-    }
 
-}
+
+    }
+    }
