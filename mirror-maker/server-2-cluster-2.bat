6717b6161a0f55aed3036c10596d08e062e3ef7c
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2021-12-21 12:30:22 +0530

    Update server-2-cluster-2.bat

--- mirror-maker/server-2-cluster-2.bat
+++ mirror-maker/server-2-cluster-2.bat
@@ -1,2 +1,2 @@
 TITLE Cluster 2 Broker 20
-%KAFKA_HOME%\bin\windows\kafka-server-start %KAFKA_HOME%\etc\kafka\server-2-cluster-2.properties
+%KAFKA_HOME%\bin\windows\kafka-server-start %KAFKA_HOME%\etc\kafka\cluster2-server0.properties
