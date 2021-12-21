64a8ce281cc32bf689c5f9259bcdc0bcbe3f6c03
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2021-12-21 18:12:45 +0530

    Update invoice-stream-aggregate-to-kafka.py

--- python-example/invoice-stream-aggregate-to-kafka.py
+++ python-example/invoice-stream-aggregate-to-kafka.py
@@ -27,6 +27,7 @@ import pyspark
 
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.master("local[*]")\
+                            .config('spark.sql.shuffle.partitions', 4)\
                             .appName("SparkStreamingKafkaInvoiceStream").getOrCreate()
 
 
