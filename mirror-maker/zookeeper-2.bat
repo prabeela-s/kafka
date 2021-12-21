2c962561f699c22029c9820bc8beac82ef70befa
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2021-12-21 12:36:25 +0530

    Update zookeeper-2.bat

--- mirror-maker/zookeeper-2.bat
+++ mirror-maker/zookeeper-2.bat
@@ -1,2 +1,2 @@
 TITLE Cluster-2-ZooKeeper 3181
-%KAFKA_HOME%\bin\windows\zookeeper-server-start %KAFKA_HOME%\etc\kafka\zookeeper-2.properties
+%KAFKA_HOME%\bin\windows\zookeeper-server-start  .\zookeeper-2.properties
